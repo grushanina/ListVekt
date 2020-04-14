ecece7315313b712046f3dea73f40ada057f5de3
grushanina <61738785+grushanina@users.noreply.github.com>
2020-04-14 17:19:46 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/List.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class List<T>
+    {
+        private T[] data;
+        public int Count { get; private set; }
+        public List()
+        {
+            data = new T[0];
+        }
+
+        public void Add(T x)
+        {
+            Array.Resize(ref data, Count + 1);
+            data[Count] = x;
+            ++Count;
+        }
+
+        public void Insert(int index,T value)
+        {
+            if (Count==0 || index>=Count-1)
+            {
+                Add(value);
+            }
+            else
+            {
+                Array.Resize(ref data, Count + 1);
+                ++Count;
+                for (int i = Count - 1; i > index; i--)
+                {
+                    data[i] = data[i - 1];
+                }
+                data[index] = value;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index<Count)
+            {
+                for (int i = index; i < Count-1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                Array.Resize(ref data, Count - 1);
+                Count--;
+            }
+        }
+
+       public void Clear()
+        {
+            Count = 0;
+        }
+
+        public T this[int key]
+        {
+            get
+            {
+                return data[key];
+            }
+
+            set
+            {
+                data[key] = value;
+            }
+
+        }
+
+        public bool Constains(Predicate<T> match)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (match(data[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {
+            Comparer<T> a = Comparer<T>.Default;
+            for (int i = 0; i < Count; i++)
+            {   
+                if (a.Compare(data[i], item) == 0)
+                {
+                    return i; 
+                }
+            }
+            return -1;
+        }
+
+        public void ForEach(System.Action<T> action)
+        {
+            for (int i = 0; i != Count; ++i)
+            {
+                action(data[i]);
+            }
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (match(data[i]))
+                    return data[i];
+            }
+            return default (T);
+        }
+
+        public int FindIndex(Predicate<T> match)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (match(data[i]))
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        delegate int tr(int x);
+        delegate int sum(int x, int y);
+        static void Main()
+        {
+            //var el = new System.Collections.Generic.List<int>() { 1, 2, 3, 4 };
+            //int od = el.Find(x => x % 2 != 0);
+            //Console.Write(od);
+
+            //Func<int, int, int> f1 = (x, y) => x * y;
+            //Action<int, int> f2 = (x, y) => { var z = x + y; Console.WriteLine("x+y" + z); };
+            //f2(5, 5);
+            //sum p = (x, y) => x + y;
+            //tr sq = x => x * x;
+            //Console.WriteLine(sq(3));
+            //Console.WriteLine(p(3,2));
+
+            var lst = new List<string>();
+            lst.Add("1");
+            lst.Add("2");
+            lst.Add("3");
+            //lst[0] = "uio";
+            //lst.Insert(1, "f");
+            //lst.RemoveAt(2);
+            //lst.Clear();
+            //for (int i = 0; i < lst.Count; i++)
+            //{
+            //    Console.WriteLine(lst[i]);
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine(lst.Constains(x => x == "2"));
+
+            //Console.WriteLine(lst.IndexOf("4"));
+
+            //lst.ForEach(n=>Console.WriteLine(n));
+
+            //Console.WriteLine(lst.Find(x => x == "2"));
+
+            Console.WriteLine(lst.FindIndex(x => x == "2"));
+        }
+    }
+}
